27e28b00cd3134526422b14ba6ae925c0cd4a592
NamBlue2005 <nam.230437@student.hactech.edu.vn>
2025-03-19 14:28:29 +0700

    sửa lại ss12

---
+++ java_web/ss11/src/main/java/repository/ProductRepository.java
@@ -0,0 +1,152 @@
+package repository;
+
+import model.Product;
+import connect.DatabaseConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductRepository implements IProductRepository {
+
+    @Override
+    public List<Product> findAll() {
+        List<Product> products = new ArrayList<>();
+        String query = "SELECT * FROM products";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                products.add(new Product(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getDouble("price"),
+                        resultSet.getString("description"),
+                        resultSet.getString("producer")
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return products;
+    }
+
+    @Override
+    public void save(Product product) {
+        String query = "INSERT INTO products (name, price, description, producer) VALUES (?, ?, ?, ?)";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+
+            statement.setString(1, product.getName());
+            statement.setDouble(2, product.getPrice());
+            statement.setString(3, product.getDescription());
+            statement.setString(4, product.getProducer());
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void update(int id, Product product) {
+        String query = "UPDATE products SET name = ?, price = ?, description = ?, producer = ? WHERE id = ?";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setString(1, product.getName());
+            statement.setDouble(2, product.getPrice());
+            statement.setString(3, product.getDescription());
+            statement.setString(4, product.getProducer());
+            statement.setInt(5, id);
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        String query = "DELETE FROM products WHERE id = ?";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Product findById(int id) {
+        String query = "SELECT * FROM products WHERE id = ?";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                return new Product(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getDouble("price"),
+                        resultSet.getString("description"),
+                        resultSet.getString("producer")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public int generateId() {
+        String query = "SELECT MAX(id) AS max_id FROM products";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            if (resultSet.next()) {
+                return resultSet.getInt("max_id") + 1;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 1;
+    }
+
+    @Override
+    public List<Product> findByName(String name) {
+        List<Product> result = new ArrayList<>();
+        String query = "SELECT * FROM products WHERE name LIKE ?";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setString(1, "%" + name + "%");
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                result.add(new Product(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getDouble("price"),
+                        resultSet.getString("description"),
+                        resultSet.getString("producer")
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
--- java_web/ss11/src/main/java/service/ProductService.java
+++ java_web/ss11/src/main/java/service/ProductService.java
@@ -1,157 +1,51 @@
 package service;
 
 import model.Product;
-import connect.DatabaseConnection;
+import repository.IProductRepository;
+import repository.ProductRepository;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ProductService implements IProductService {
+    private final IProductRepository productRepository = new ProductRepository();
 
     @Override
     public List<Product> findAll() {
-        List<Product> products = new ArrayList<>();
-        String query = "SELECT * FROM products";
-
-        try (Connection connection = DatabaseConnection.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query);
-             ResultSet resultSet = statement.executeQuery()) {
-
-            while (resultSet.next()) {
-                products.add(new Product(
-                        resultSet.getInt("id"),
-                        resultSet.getString("name"),
-                        resultSet.getDouble("price"),
-                        resultSet.getString("description"),
-                        resultSet.getString("producer")
-                ));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return products;
+        return productRepository.findAll();
     }
 
     @Override
     public void add(Product product) {
-        save(product);
+        productRepository.save(product);
     }
 
     @Override
     public void save(Product product) {
-        String query = "INSERT INTO products (name, price, description, producer) VALUES (?, ?, ?, ?)";
-
-        try (Connection connection = DatabaseConnection.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
-
-            statement.setString(1, product.getName());
-            statement.setDouble(2, product.getPrice());
-            statement.setString(3, product.getDescription());
-            statement.setString(4, product.getProducer());
-
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        productRepository.save(product);
     }
 
     @Override
     public void update(int id, Product product) {
-        String query = "UPDATE products SET name = ?, price = ?, description = ?, producer = ? WHERE id = ?";
-
-        try (Connection connection = DatabaseConnection.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query)) {
-
-            statement.setString(1, product.getName());
-            statement.setDouble(2, product.getPrice());
-            statement.setString(3, product.getDescription());
-            statement.setString(4, product.getProducer());
-            statement.setInt(5, id);
-
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        productRepository.update(id, product);
     }
 
     @Override
     public void delete(int id) {
-        String query = "DELETE FROM products WHERE id = ?";
-
-        try (Connection connection = DatabaseConnection.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query)) {
-
-            statement.setInt(1, id);
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        productRepository.delete(id);
     }
 
     @Override
     public Product findById(int id) {
-        String query = "SELECT * FROM products WHERE id = ?";
-        try (Connection connection = DatabaseConnection.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query)) {
-
-            statement.setInt(1, id);
-            ResultSet resultSet = statement.executeQuery();
-
-            if (resultSet.next()) {
-                return new Product(
-                        resultSet.getInt("id"),
-                        resultSet.getString("name"),
-                        resultSet.getDouble("price"),
-                        resultSet.getString("description"),
-                        resultSet.getString("producer")
-                );
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return productRepository.findById(id);
     }
 
     @Override
     public int generateId() {
-        String query = "SELECT MAX(id) AS max_id FROM products";
-        try (Connection connection = DatabaseConnection.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query);
-             ResultSet resultSet = statement.executeQuery()) {
-
-            if (resultSet.next()) {
-                return resultSet.getInt("max_id") + 1;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return 1;
+        return productRepository.generateId();
     }
 
     @Override
     public List<Product> findByName(String name) {
-        List<Product> result = new ArrayList<>();
-        String query = "SELECT * FROM products WHERE name LIKE ?";
-
-        try (Connection connection = DatabaseConnection.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query)) {
-
-            statement.setString(1, "%" + name + "%");
-            ResultSet resultSet = statement.executeQuery();
-
-            while (resultSet.next()) {
-                result.add(new Product(
-                        resultSet.getInt("id"),
-                        resultSet.getString("name"),
-                        resultSet.getDouble("price"),
-                        resultSet.getString("description"),
-                        resultSet.getString("producer")
-                ));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return result;
+        return productRepository.findByName(name);
     }
 }
